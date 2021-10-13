db26989e30c5c0334a3d10f4a2962caa76a92c6d
Gerardo-Castellanos <gerardo.castellanos@ibm.com>
2021-10-13 01:40:49 -0500

    login code refract

--- functions/auth/login.py
+++ functions/auth/login.py
@@ -10,27 +10,23 @@ database = Database()
 def lambda_handler(event, context):
 
     payload = json.loads(event['body'])
+    unauthorized_response = {
+            "statusCode": 401,
+            "body": json.dumps({
+                "message": "unauthorized"
+            })
+        }
 
     try:
         user = payload['username']
         password = payload['password']
     except (ValueError, KeyError):
-        return {
-            "statusCode": 401,
-            "body": json.dumps({
-                "message": "unauthorized"
-            })
-        }
+        return unauthorized_response
 
     is_authorized = database.validate_user_password(user, password)
 
     if not is_authorized:
-        return {
-            "statusCode": 401,
-            "body": json.dumps({
-                "message": "unauthorized"
-            })
-        }
+        return unauthorized_response
 
     query_result = database.get_role_by_username('admin')
     token = token_functions.generate_token(query_result)
