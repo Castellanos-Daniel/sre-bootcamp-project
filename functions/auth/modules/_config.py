db417c602d6cfa51cf30d235dca695919150fd6a
Gerardo-Castellanos <gerardo.castellanos@ibm.com>
2021-10-23 13:15:08 -0500

    Fixed unittests issues

--- functions/auth/modules/_config.py
+++ functions/auth/modules/_config.py
@@ -13,19 +13,23 @@ class Config:
         secret_name = getenv('DbSecret')
         region_name = getenv('AwsRegion')
 
-        try:
-            session = boto3.session.Session()
-            client = session.client(
-                service_name='secretsmanager',
-                region_name=region_name
-            )
-
-            get_secret_value_response = client.get_secret_value(
-                SecretId=secret_name
-            )
-        except ClientError as client_error:
-            raise client_error
+        signarute = getenv('SIGNATURE')
+        if signarute:
+            self.SIGNATURE = signarute
         else:
-            secret = json.loads(get_secret_value_response['SecretString'])
+            try:
+                session = boto3.session.Session()
+                client = session.client(
+                    service_name='secretsmanager',
+                    region_name=region_name
+                )
 
-        self.SIGNATURE=secret['secretKey']
+                get_secret_value_response = client.get_secret_value(
+                    SecretId=secret_name
+                )
+            except ClientError as client_error:
+                raise client_error
+            else:
+                secret = json.loads(get_secret_value_response['SecretString'])
+
+            self.SIGNATURE=secret['secretKey']
