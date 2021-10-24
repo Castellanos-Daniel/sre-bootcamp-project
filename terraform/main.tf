b6a08cdf14fb5d695e098edec4da4cb47d4a6d7b
Gerardo-Castellanos <gerardo.castellanos@ibm.com>
2021-10-23 19:42:58 -0500

    decouple providers, outputs and datasources from main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,58 +1,16 @@
-provider "aws" {
-  region  = "us-east-2"
-}
-terraform {
-  backend "s3" {
-    bucket  = "daniel-terraform-backend"
-    key     = "project/terraform.tfstate"
-    region  = "us-east-2"
-    encrypt = true
-  }
-}
 
 module "my_vpc" {
   source = "./modules/vpc"
 }
 
 module "rds_db" {
-  source = "./modules/db"
-  subnets = module.my_vpc.private_subnets
+  source        = "./modules/db"
+  subnets       = module.my_vpc.private_subnets
   instance_name = "terraform-instance"
 }
 
 module "bastion_host" {
-  source = "./modules/bastion_host"
+  source    = "./modules/bastion_host"
   subnet_id = module.my_vpc.public_subnet[0]
-  vpc_ids = data.aws_vpcs.created_vpc.ids
-}
-
-data "aws_vpcs" "created_vpc" {
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-  depends_on = [
-    module.my_vpc
-  ]
-}
-data "aws_caller_identity" "current" {}
-
-output "vpc_public_subnet" {
-  value = module.my_vpc.public_subnet
-}
-
-output "db_endpoint" {
-  value = module.rds_db.db_endpoint
-}
-
-output "vpc_id" {
-  value = data.aws_vpcs.created_vpc.ids
-}
-
-output "db_identifier" {
-  value = module.rds_db.identifier
-}
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
+  vpc_ids   = data.aws_vpcs.created_vpc.ids
 }
