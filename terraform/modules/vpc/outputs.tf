2644416436a7882ec6aa5b63016b47f011bdc5ab
Gerardo-Castellanos <gerardo.castellanos@ibm.com>
2021-10-25 09:37:08 -0500

    Added security group in custom vpc, fixex vpc module output

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -6,9 +6,9 @@ output "public_subnet" {
   value = module.vpc.public_subnets
 }
 
-# output "vpc_id" {
-#   value = module.vpc.id
-# }
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
 
 output "vpc_name" {
   value = module.vpc.name
