695fc4159ffb9eee5784935ccc7a76e933b8ba7d
Gerardo-Castellanos <gerardo.castellanos@ibm.com>
2021-10-25 09:39:51 -0500

    Adding missing file

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,17 +3,30 @@ module "my_vpc" {
   source = "./modules/vpc"
 }
 
+module "vpc_sg" {
+  source = "./modules/lambda/vpc_security_group"
+  vpc_id = module.my_vpc.vpc_id
+}
+
 module "rds_db" {
   source        = "./modules/db"
+  vpc_id        = module.my_vpc.vpc_id
   subnets       = module.my_vpc.private_subnets
   instance_name = "terraform-instance"
-  master_user = var.db_master_user
-  master_pass = var.db_master_pass
+  master_user   = var.db_master_user
+  master_pass   = var.db_master_pass
+  access_allowed_sg  = [ module.vpc_sg.vpc_sg_id ]
 }
 
 module "lambda_deps_layer" {
-  source = "./modules/lambda/layer"
-  name = "DepsLayer"
+  source      = "./modules/lambda/layer"
+  name        = "DepsLayer"
   bucket_name = "deps-layer-bucket"
-  filename = "deps.zip"
+  filename    = "deps.zip"
+}
+
+module "lambda_vpc_execution_role" {
+  source = "./modules/lambda/vpc_execution_role"
+  name = "lambda_db_access_role"
+  db_username = "lambda-user"
 }
