c0e81893f7b390f90aa4f19f790ffee273994f5f
Gerardo-Castellanos <gerardo.castellanos@ibm.com>
2021-10-28 15:07:50 -0500

    Deleted a few comments

--- terraform-prod-deployment/main.tf
+++ terraform-prod-deployment/main.tf
@@ -4,30 +4,11 @@ data "aws_api_gateway_rest_api" "dev_api" {
 
 resource "aws_api_gateway_deployment" "prod_deployment" {
   rest_api_id = data.aws_api_gateway_rest_api.dev_api.id
-
   # triggers = {
   #   redeployment = sha1(jsonencode([
-
-  #     module.health_check_root_endpoint.method_id,
-  #     module.health_check_root_endpoint.integration_id,
-
-  #     module.health_check_endpoint.resource_id,
-  #     module.health_check_endpoint.method_id,
-  #     module.health_check_endpoint.integration_id,
-
-  #     module.login_endpoint.resource_id,
-  #     module.login_endpoint.method_id,
-  #     module.login_endpoint.integration_id,
-
-  #     module.cidr_to_mask_endpoint.resource_id,
-  #     module.cidr_to_mask_endpoint.method_id,
-  #     module.cidr_to_mask_endpoint.integration_id,
-
-  #     module.mask_to_cidr_endpoint.resource_id,
-  #     module.mask_to_cidr_endpoint.method_id,
-  #     module.mask_to_cidr_endpoint.integration_id,
+      
   #   ]))
-  # }
+  }
 
   lifecycle {
     create_before_destroy = true
