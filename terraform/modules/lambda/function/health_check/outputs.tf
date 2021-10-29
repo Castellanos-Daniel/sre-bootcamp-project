d509df304c1179be0af8a404fc18fedeb9c1dc12
Gerardo-Castellanos <gerardo.castellanos@ibm.com>
2021-10-29 09:54:57 -0500

    worked on funtions outputs, added dev aliases

--- terraform/modules/lambda/function/health_check/outputs.tf
+++ terraform/modules/lambda/function/health_check/outputs.tf
@@ -1,9 +1,13 @@
 output "invoke_arn" {
-    value = module.health_check.lambda_function_invoke_arn
+  value = module.health_check.lambda_function_invoke_arn
 }
 
 output "function_name" {
-    value = module.health_check.lambda_function_name
+  value = module.health_check.lambda_function_name
+}
+
+output "function_version" {
+  value = module.health_check.lambda_function_version
 }
 
 output "alias_invoke_arn" {
--- terraform/modules/lambda/function/login/outputs.tf
+++ terraform/modules/lambda/function/login/outputs.tf
@@ -1,7 +1,19 @@
 output "invoke_arn" {
-    value = module.login_function.lambda_function_invoke_arn
+  value = module.login_function.lambda_function_invoke_arn
 }
 
 output "function_name" {
-    value = module.login_function.lambda_function_name
+  value = module.login_function.lambda_function_name
+}
+
+output "function_version" {
+  value = module.login_function.lambda_function_version
+}
+
+output "alias_invoke_arn" {
+  value = module.login_alias_dev.lambda_alias_invoke_arn
+}
+
+output "lambda_alias_name" {
+  value = module.login_alias_dev.lambda_alias_name
 }
